053d48633f545a8567b7da354f0e2dc3e4e32906
amir <amiraziz8454@gmail.com>
2025-03-25 12:58:11 +0500

    pw generater

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,34 @@
 import React from 'react'
 import './App.css'
 import { useState, useCallback, useEffect, useRef } from 'react'
-function App() {
- 
-  const[length, setlength] = useState("8")
-  const[numAllowed, setnumAllowed] = useState(false)
-  const[charAllowed, setcharAllowed] = useState(false)
-  const[password, setpassword] = useState("")
-  
-  //useRef hook
-  // its hooks base 
-  const passwordRef = useRef(null)
 
-  let pass = ""
-let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-if(numAllowed) str += "0123456789"
-if(numAllowed) str += "!@#$%^&*(){}[]|\/~`"
-for(let i=1;i<=length;i++){
-  let char = Math.floor(Math.random()*str.length+1)
-  pass += str.charAt(char)
-}
-setPassword(pass)
-console.log(pass)
-},[length, numberAllowed,charAllowed,setPassword])
-
-useEffect(()=> {
-  passwordGenerator()
-},[length, numberAllowed,charAllowed,setPassword])
-
-
-
+  function App() {
+    const [length,setlength] = useState("8")
+    const [num,setNum] = useState(false)
+    const [char,setChar] = useState(false)
+    const [Password,setpassword] = useState("")
 
   return (
-    <div className='app' >
-
-      <h1>password generator</h1>
+    <div className='app'>
+      <h1>Password Genertor{Password}</h1>
+     
       <br />
-      <label htmlFor="pw">password</label>
-      <input type="text" name="pw" value={password} onChange={(e)=>setpassword(e.target.value)} />
-      <button type="submit">copy</button>
-     
-      <input type="range" name="length" id="" value={length} onChange={(e)=>setlength(e.target.value)}/>
-      <label htmlFor="length">length</label>{length}
-      
+      <input type="text" name="pw" /*value={Password} onChange ={(e)=> setPassword(e.target.value)}*/   />
+
+      <button type='submit'>copy</button>
+
+      <input type="range" name="" id="" /*value={length} onChange={(e)=> setLength(e.target.value)}*/ />
+      {length}
       <input type="checkbox" name="num" id="" />
-      <label htmlFor="num">num</label>
-     
+      num
       <input type="checkbox" name="char" id="" />
-      <label htmlFor="char">char</label>
+      char
+
+
     </div>
   )
+
+  
 }
 
 export default App
